6c23ee9dcaf37e430bd33a6343e707e3117c2e2c
Amir <amirseraj.ir@gmail.com>
2022-02-11 10:52:50 +0330

    Add styled-components and router, set routes, make Logo as a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/landing" element={Landing} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
