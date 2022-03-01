8eff2b3474e3137456642e91fbb977668aeb1166
Amir <amirseraj.ir@gmail.com>
2022-03-01 15:04:36 +0330

    Wire up protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Landing, Error, Register } from "./pages/index";
 import {
-  AddJob,
   AllJobs,
+  AddJob,
   Profile,
   SharedLayout,
   Stats,
 } from "./pages/dashboard/index";
 
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
---
+++ client/src/pages/ProtectedRoute.js
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAppContext();
+  if (!user) {
+    return <Navigate to="/landing"></Navigate>;
+  }
+  return children;
+}
+
+export default ProtectedRoute;
